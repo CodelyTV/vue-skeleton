66f8a3be34459c5e08d8c477bb82b8b1abc92684
Antonio Gil <antonioluisgil@gmail.com>
2023-03-22 18:42:44 +0100

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+const msg = ref('Welcome to Your Vue.js + TypeScript App')
+</script>
+
+<template>
+  <h1>Home</h1>
+  <HelloWorld :msg="msg" />
+</template>

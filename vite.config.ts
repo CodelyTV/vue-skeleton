95d0bde9f30ba0ff74fb8612a1af214b5beb9930
Antonio Giroz <antonioluisgil@gmail.com>
2023-04-04 15:36:32 +0200

    feat: add skeleton component

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,33 @@
-import { URL, fileURLToPath } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+      entryRoot: 'src',
+      outputDir: 'dist/types',
+    }),
+    vue(),
+    cssInjectedByJsPlugin(),
+  ],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es', 'cjs'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
     },
   },
+
 })
